497bbc9727eceb088552188be28434798e54cc90
Kiwane Garner <kiwanegarner012@yahoo.com>
2018-04-13 12:39:31 -0700

    added Youtube clone project

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PeakFitness from "../images/PeakFitness.png";
-import ViciousCycles from "../images/ViciousCycles.png";
+import YoutubeClone from "../images/YoutubeClone.png";
 import YelpCamp from "../images/YelpCamp.png";
 
 
@@ -14,22 +14,22 @@ class ProjectsPage extends Component {
         <section className="project-section">
           <div className="row">
             <section className="col-md-5 offset-md-1 img-thumbnail text-center">
+              <h3>Youtube Clone</h3>
+              <img src={YoutubeClone} className="project-img" alt="Youtube Clone" />
+              <p><strong>Technologies Used:</strong> HTML, CSS, Bootstrap 4, React, Node, and Express</p>
+              <a href="https://kgyoutubeapp.herokuapp.com/" target="_blank" className="btn btn-secondary">View Full Site</a>
+            </section>
+            <section className="col-md-5  img-thumbnail text-center ">
               <h3>Yelp Camp</h3>
               <img src={YelpCamp} className="project-img" alt="Yelp Camp" />
               <p><strong>Technologies Used:</strong> HTML, CSS, Bootstrap 3, EJS, Node, Express, and MongoDB</p>
               <a href="https://ancient-headland-83860.herokuapp.com/" target="_blank" className="btn btn-secondary">View Full Site</a>
             </section>
-            <section className="col-md-5  img-thumbnail text-center ">
-              <h3>Project Title</h3>
+            <section className="col-md-5 offset-md-1 img-thumbnail text-center ">
+              <h3>Peak Fitness</h3>
               <img src={PeakFitness} className="project-img" alt="Peak Fitness" />
               <p><strong>Technologies Used:</strong> HTML5, CSS3, Adobe Dreamweaver</p>
-              <a href="" target="_blank" className="btn btn-secondary">View Full Site</a>
-            </section>
-            <section className="col-md-5 offset-md-1 img-thumbnail text-center ">
-              <h3>Project Title</h3>
-              <img src={ViciousCycles} className="project-img" alt="Vicious Cycles" />
-              <p><strong>Technologies Used:</strong> HTML5, CSS3, Adobe Dreamweaver</p>
-              <a href="http://wcet2.waketech.edu/kmgarner/WEB140/viciousCycles/index.html" target="_blank" className="btn btn-secondary">View Full Site</a>
+              <a href="http://wcet2.waketech.edu/kmgarner/WEB140/peakFitness/index.html" target="_blank" className="btn btn-secondary">View Full Site</a>
             </section>
           </div>
         </section>
