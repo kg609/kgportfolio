56ae9ada2ad18cfa5cfbd206e247db6911af7d6b
Kiwane Garner <kiwanegarner@Kiwanes-MacBook-Pro.local>
2018-11-26 23:56:20 -0500

    Added a carousel to the home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,8 +5,28 @@ class HomePage extends Component {
   render() {
     return (
       <div className="HomePage" id="HomePage">
-        <h1>With knowledge, <br/>
-        passion, heart, and soul</h1>
+
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+            <h1>With knowledge, <br/> passion, heart, and soul</h1>
+            </div>
+            <div className="carousel-item">
+              <h1>Quote or Phrase 2</h1>
+            </div>
+            <div className="carousel-item">
+              <h1>Quote or Phrase 3</h1>
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
 
         <section className="img-section">
           <div className="row">
@@ -34,3 +54,4 @@ class HomePage extends Component {
 }
 
 export default HomePage;
+
