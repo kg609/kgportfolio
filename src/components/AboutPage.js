c1581467bf1f17a3e391fe76d8fb8cb3a1209c4f
Kiwane Garner <kiwanegarner@Kiwanes-MacBook-Pro.local>
2018-11-28 16:58:53 -0500

    recentered some text and images

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -10,10 +10,10 @@ class AboutPage extends Component {
 
           <section className="AboutContent">
             <div className="row">
-              <section className="col-md-6 AboutMe">
+              <section className="col-md-5 offset-md-1 AboutMe">
                 <p>My name is Kiwane Garner and I am a self-taught devloper currently attending Wake Tech Community college in pursuit of a web developer degree. So far on my journey to becoming a professional web developer I have learned and become efficient in languages such as HTML5, CSS3, Bootstrap3, Javascript, and React. Although i have become effiecent in using those languages The list or the journey doesn’t end there. I will be continuing to improve my skills in the languages mentioned before and also increase my knowledge and exposure to other languages such as php, java, mysql, Angular, and SASS.</p>
               </section>
-              <section className="col-md-5 offset-md-1 myImg">
+              <section className="col-md-5 myImg">
 
               </section>
             </div>
