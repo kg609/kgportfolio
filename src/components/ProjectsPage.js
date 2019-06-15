b623e148382a04b605c67f1959c2cec7e085ddb5
clear <kiwanegarner@Kiwanes-MacBook-Pro.local>
2019-06-15 11:22:16 -0400

    Removed two old projects and added two new projects to the projects section

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import YoutubeClone from "../images/YoutubeClone.png";
 import PortfolioDemo from "../images/portfolioDemo.png";
-import YelpCamp from "../images/YelpCamp.png";
 import KgPrimeSteak from "../images/KgPrimeSteak.png";
-import KgWebBlogs from "../images/KgWebBlogs.png";
+import HenrysRestaurant from "../images/henrysRestaurant.png";
+import CarolinaAleHouse from "../images/CarolinaAleHouse.png";
 
 
 
@@ -59,26 +59,34 @@ class ProjectsPage extends Component {
         <section className="project-section">
           <div className="row">
             <section className="col-md-5 offset-md-1 img-thumbnail text-center">
-              <h3>Youtube Clone</h3>
-              <img src={YoutubeClone} className="project-img" alt="Youtube Clone" />
-              <p><strong>Technologies: </strong> HTML, CSS, Bootstrap 4, React, Node, and Express</p>
-              <a href="https://kgyoutubeapp.herokuapp.com/" target="_blank" className="btn btn-project">View Full Site</a>
-              <a href="https://github.com/kg609/Youtube-Clone" target="_blank" className="btn btn-github">View Github</a>
+              <h3>Henry's Restaurant</h3>
+              <img src={HenrysRestaurant} className="project-img" alt="Youtube Clone" />
+              <p><strong>Technologies: </strong> HTML, CSS, JQuery, and WordPress, and Php</p>
+              <a href="https://www.henrysrestaurant.com/" target="_blank" className="btn btn-project">View Full Site</a>
+              {/* <a href="https://github.com/kg609/Youtube-Clone" target="_blank" className="btn btn-github">View Github</a> */}
             </section>
             <section className="col-md-5  img-thumbnail text-center ">
               <h3>Website Portfolio</h3>
               <img src={PortfolioDemo} className="project-img" alt="Yelp Camp" />
               <p><strong>Technologies: </strong> HTML, CSS, Bootstrap 3, JQuery, AWS</p>
               <a href="https://tinyurl.com/kiwanegarner" target="_blank" className="btn btn-project">View Full Site</a>
-              <a href="#" target="_blank" className="btn btn-github">View Github</a>
+              {/* <a href="#" target="_blank" className="btn btn-github">View Github</a> */}
             </section>
             <section className="col-md-5 offset-md-1 img-thumbnail text-center ">
+              <h3>Carolina Ale House</h3>
+              <img src={CarolinaAleHouse} className="project-img" alt="KG Prime Steak" />
+              <p><strong>Technologies: </strong> HTML, CSS, JQuery, WordPress, and Php</p>
+              <a href="https://www.carolinaalehouse.com/" target="_blank" className="btn btn-project">View Full Site</a>
+              {/* <a href="https://github.com/kg609/KG-Prime-Steak" target="_blank" className="btn btn-github">View Github</a> */}
+            </section>
+
+            {/* <section className="col-md-5 offset-md-1 img-thumbnail text-center ">
               <h3>KG Prime Steak</h3>
               <img src={KgPrimeSteak} className="project-img" alt="KG Prime Steak" />
               <p><strong>Technologies: </strong> HTML5, CSS3, Sass, Hyperapp</p>
               <a href="https://kgprimesteak.herokuapp.com/" target="_blank" className="btn btn-project">View Full Site</a>
               <a href="https://github.com/kg609/KG-Prime-Steak" target="_blank" className="btn btn-github">View Github</a>
-            </section>
+            </section> */}
           </div>
         </section>
       </div>
