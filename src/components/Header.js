2e48a21cf6114ade05cbae07e9508c79de7e24e1
Kiwane Garner <kiwanegarner012@yahoo.com>
2019-09-04 21:34:16 -0400

    Added smooth scroll animation to the navigation menu and also changed the first quote

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,72 @@
 import React, { Component } from 'react';
+import { TweenLite } from "gsap";
+import { Link, animateScroll as scroll } from "react-scroll";
+
 // import resume from '../images/Kiwane_Garner_Resume.pdf';
 
 
+
 class Header extends Component {
+constructor(props){
+  super(props);
+
+ 
+}
+
+scrollToTop = () => {
+    scroll.scrollToTop(); 
+};
+
+componentDidMount(){
+ 
+}
+
   render() {
     return (
       <div className="Header" id="HeaderPage">
         <header>
           <nav className="navbar fixed-top navbar-expand-lg">
-            <a className="navbar-brand" href="#HeaderPage">Kiwane Garner</a>
+            <h1 className="navbar-brand" onClick={this.scrollToTop}>Kiwane Garner</h1>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <i className="fa fa-bars"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="#HeaderPage">Home</a>
+                <Link
+                      className="nav-link"
+                      activeClass="active"
+                      to="HeaderPage"
+                      spy={true}
+                      smooth={true}
+                      offset={0}
+                      duration= {500}
+                  >Home</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#AboutPage">About</a>
+                  <Link
+                      className="nav-link"
+                      activeClass="active"
+                      to="AboutPage"
+                      spy={true}
+                      smooth={true}
+                      offset={0}
+                      duration= {500}
+                  >About</Link>
                 </li>
                 {/* <li className="nav-item">
                   <a className="nav-link" href={resume} download="Kiwane_Garner_Resume">Resume</a>
                 </li> */}
                 <li className="nav-item">
-                  <a className="nav-link" href="#ProjectsPage">Projects</a>
+                <Link
+                      className="nav-link"
+                      activeClass="active"
+                      to="ProjectsPage"
+                      spy={true}
+                      smooth={true}
+                      offset={0}
+                      duration= {500}
+                  >Projects</Link>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="https://kmgarner2017.wordpress.com/" target="_blank">Blogs</a>
